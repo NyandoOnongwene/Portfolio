8950880696b38312ec98f4d956acc3124f2046a0
Nyando Onongwene <nyandoonotex@gmail.com>
2023-05-15 11:02:58 +0100

    inserting codes for the component files

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,12 +1,37 @@
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    <h1>Hello World</h1>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//     <h1>Hello World</h1>
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
+import Header from './components/header/Header'
+import Navbar from './components/navbar/Navbar'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Services from './components/services/Services'
+import Portfolio from './components/portfolio/Portfolio'
+import Testimonial from './components/testimonial/Testimonial'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
+function App() {
+return (
+<>
+<Header/>
+<Navbar/>
+<About/>
+<Experience/>
+<Services/>
+<Portfolio/>
+<Testimonial/>
+<Contact/>
+<Footer/>
+</>
+);
+}
+export default App; 
