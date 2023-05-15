41b7235d8af3ab81da2dfe1d404c58ed5089681b
Nyando Onongwene <nyandoonotex@gmail.com>
2023-05-15 11:59:37 +0100

    completed importing components to the App.js file.

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -12,15 +12,26 @@ import './App.css';
 // export default App;
 
 import Header from './components/header/Header';
+import Navbar from './components/navbar/Navbar';
 import About from './components/about/About';
+import Experience from './components/experience/Experience';
+import Services from './components/services/Services';
+import Portfolio from './components/portfolio/Portfolio';
+import Testimonial from './components/testimonial/Testimonial';
 import Contact from './components/contact/Contact';
-
+import Footer from './components/footer/Footer';
 function App() {
 return (
 <div>
   <Header />
+  <Navbar />
   <About />
+  <Experience />
+  <Services />
+  <Portfolio />
+  <Testimonial />
   <Contact />
+  <Footer />
 </div>
 );
 }
---
+++ portfolio/src/components/services/Services.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Services() {
+  return (
+    <div>Services</div>
+  )
+}
+
