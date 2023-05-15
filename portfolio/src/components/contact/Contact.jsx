6740dbfd281793406b7d3009c90cc5e9f84820f4
Nyando Onongwene <nyandoonotex@gmail.com>
2023-05-15 15:11:02 +0100

    contact modified

--- portfolio/src/components/contact/Contact.jsx
+++ portfolio/src/components/contact/Contact.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+// import React from 'react'
 
-export default function Contact() {
-  return (
-    <div>Contact</div>
-  )
-}
+// export default function Contact() {
+//   return (
+//     <div>Contact</div>
+//   )
+// }
 
 // import React from 'react'
 // import './contact.css'
@@ -12,7 +12,7 @@ export default function Contact() {
 // import {AiOutlineLinkedin} from 'react-icons/ai'
 // import {BsWhatsapp} from 'react-icons/bs'
 // import {useRef} from 'react'
-// import emailjs from 'emailjs-com'
+
 // const Contact = () => {
 // const form = useRef();
 // const sendEmail = (e) => {
@@ -63,3 +63,73 @@ export default function Contact() {
 // }
 // export default Contact
 
+import React from 'react'
+import './contact.css'
+import {MdOutlineEmail} from 'react-icons/md'
+import {AiOutlineLinkedin} from 'react-icons/ai'
+import {BsWhatsapp} from 'react-icons/bs'
+import {useRef} from 'react'
+
+const Contact = () => {
+const form = useRef();
+const sendEmail = (e) => {
+e.preventDefault();
+// emailjs.sendForm('service_5vuauza', 'template_fdnnl56', form.current,
+// 'EmSdUNQzQuxkUqGWx')
+e.target.reset()
+.then((result) => {
+console.log(result.text)
+},(error) =>{
+console.log(error.text);
+});
+};
+return (
+
+<section id="contact">
+<h5>Get in touch</h5>
+<h2>Contact me</h2>
+<div className="container contact__container">
+<div className="contact__options">
+<article className='contact__option'>
+<MdOutlineEmail className='contact__option__icon'/>
+
+<h4>Email</h4>
+<h5>nyandoonotex@gmail.com</h5>
+<a href='mailto:nyandoonotex@gmail.com' target='_blank' >send a message</a>
+</article>
+<article className='contact__option'>
+<AiOutlineLinkedin className='contact__option__icon'/>
+
+<h4>LinkedIn</h4>
+<h5>Profile</h5>
+<a href='https://www.linkedin.com/in/nyando-onongwene-321210228'
+target='_blank'>Tap me on LinkedIn</a>
+</article>
+<article className='contact__option'>
+<BsWhatsapp className='contact__option__icon'/>
+
+<h4>Whatsapp</h4>
+<h5>Direct message</h5>
+<a href='https://web.whatsapp.com/send?phone=+237675831475'
+target='_blank'>Whatsapp me</a>
+</article>
+</div>
+
+<form ref={form} onSubmit={sendEmail}>
+    
+<input type="text" name='name' placeholder='full name' required/>
+
+
+<input type="email" name='email' placeholder='your email' required/>
+<textarea name="message" id="message" cols="30" rows="10"
+placeholder='your message' required></textarea>
+
+<div>
+<button type="submit" className='btn btn-primary'>send message</button>
+</div>
+</form>
+</div>
+</section>
+)
+}
+export default Contact
