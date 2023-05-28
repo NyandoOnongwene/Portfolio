cce06b4fde8faae46ec2c0efcf2d0d6810650b5b
Nyando Onongwene <nyandoonotex@gmail.com>
2023-05-28 22:00:05 +0100

    installing swiperjs

--- portfolio/src/components/testimonial/Testimonial.jsx
+++ portfolio/src/components/testimonial/Testimonial.jsx
@@ -1,8 +1,69 @@
 import React from 'react'
 import './testimonial.css'
+import T1 from '../../assets/daddy.jpg'
+import T2 from '../../assets/mom.jpg'
+import T3 from '../../assets/Mbone.jpg'
+
+const data = [
+    {
+      id: 1,
+      review:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus est, consequatur illum ullam numquam voluptate necessitatibus sequi quidem obcaecati! Veritatis repellendus provident neque. Porro, id illum iure soluta vel alias, maiores tempora doloremque vitae neque maxime at, facilis ipsum molestiae dicta quasi harum optio atque deserunt quis amet consectetur perspiciatis.",
+      avatar: T1,
+      name: "Mbo Valentine",
+    },
+    {
+      id: 2,
+      review:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus est, consequatur illum ullam numquam voluptate necessitatibus sequi quidem obcaecati! Veritatis repellendus provident neque. Porro, id illum iure soluta vel alias, maiores tempora doloremque vitae neque maxime at, facilis ipsum molestiae dicta quasi harum optio atque deserunt quis amet consectetur perspiciatis.",
+      avatar: T2,
+      name: "Mbah Elsie",
+    },
+    {
+      id: 3,
+      review:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus est, consequatur illum ullam numquam voluptate necessitatibus sequi quidem obcaecati! Veritatis repellendus provident neque. Porro, id illum iure soluta vel alias, maiores tempora doloremque vitae neque maxime at, facilis ipsum molestiae dicta quasi harum optio atque deserunt quis amet consectetur perspiciatis.",
+      avatar: T3,
+      name: "Arrah Mbone",
+    },
+    {
+      id: 4,
+      review:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus est, consequatur illum ullam numquam voluptate necessitatibus sequi quidem obcaecati! Veritatis repellendus provident neque. Porro, id illum iure soluta vel alias, maiores tempora doloremque vitae neque maxime at, facilis ipsum molestiae dicta quasi harum optio atque deserunt quis amet consectetur perspiciatis.",
+      avatar: T2,
+      name: "Njang Elsie",
+
+    },
+  ];
+
 const Testimonial = () => {
   return (
-    <section id='testimonials'>Testimonial</section>
+    <section id='testimonials'>
+      <h5>review from Clients</h5>
+      <h2>Testimonials</h2>
+
+      <div className="container testimonials__container">
+        {
+          data.map(({avatar, name, review}, index) => {
+          return(
+         
+        <article  key={index} className="testimonial">
+          <div className="client__avatar">
+            <img src={avatar}/>
+         </div>
+            <h5 className='client__name'>{name}</h5>
+            <small className='client__review'>
+            {review}
+            </small>
+          
+        </article>
+          )
+           })
+         
+         }
+        
+      </div>
+    </section>
   )
 }
 
