c638521ad1ffed5c95b7ddf111e7b5a65b8f084d
Nyando Onongwene <nyandoonotex@gmail.com>
2023-06-24 07:32:35 +0100

    testimonial backend connected successfully

--- portfolio/src/components/testimonial/Testimonial.jsx
+++ portfolio/src/components/testimonial/Testimonial.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios';
 import './testimonial.css'
-import T1 from '../../assets/daddy.jpg'
-import T2 from '../../assets/mom.jpg'
-import T3 from '../../assets/Mbone.jpg'
-
-// import Swiper core and required modules
-import { Pagination } from 'swiper';
+import { Pagination} from 'swiper';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 
@@ -14,73 +10,61 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 
 
-const data = [
-    {
-      id: 1,
-      review:
-        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus est, consequatur illum ullam numquam voluptate necessitatibus sequi quidem obcaecati! Veritatis repellendus provident neque. Porro, id illum iure soluta vel alias, maiores tempora doloremque vitae neque maxime at, facilis ipsum molestiae dicta quasi harum optio atque deserunt quis amet consectetur perspiciatis.",
-      avatar: T1,
-      name: "Mbo Valentine",
-    },
-    {
-      id: 2,
-      review:
-        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus est, consequatur illum ullam numquam voluptate necessitatibus sequi quidem obcaecati! Veritatis repellendus provident neque. Porro, id illum iure soluta vel alias, maiores tempora doloremque vitae neque maxime at, facilis ipsum molestiae dicta quasi harum optio atque deserunt quis amet consectetur perspiciatis.",
-      avatar: T2,
-      name: "Mbah Elsie",
-    },
-    {
-      id: 3,
-      review:
-        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus est, consequatur illum ullam numquam voluptate necessitatibus sequi quidem obcaecati! Veritatis repellendus provident neque. Porro, id illum iure soluta vel alias, maiores tempora doloremque vitae neque maxime at, facilis ipsum molestiae dicta quasi harum optio atque deserunt quis amet consectetur perspiciatis.",
-      avatar: T3,
-      name: "Arrah Mbone",
-    },
-    {
-      id: 4,
-      review:
-        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus est, consequatur illum ullam numquam voluptate necessitatibus sequi quidem obcaecati! Veritatis repellendus provident neque. Porro, id illum iure soluta vel alias, maiores tempora doloremque vitae neque maxime at, facilis ipsum molestiae dicta quasi harum optio atque deserunt quis amet consectetur perspiciatis.",
-      avatar: T2,
-      name: "Njang Elsie",
 
-    },
-  ];
+const Testimonials = () => {
+    const [data, setData] = useState([]);
 
-const Testimonial = () => {
+    useEffect(()=> {
+        axios.get("https://spotless-cyan-kit.cyclic.app/api/testimonial").then(response => {
+            const formattedData = response.data.data.map(item => ({
+                id:item._id,
+                avatar: item.avatar,
+                name: item.name,
+              
+                review: item.review
+            }));
+            setData(formattedData)
+        }).catch(error => {
+            console.log("Error fetching portfolio data:", error);
+        })
+    }, [])
+
+
   return (
     <section id='testimonials'>
-      <h5>review from Clients</h5>
+      <h5>Review from clients</h5>
       <h2>Testimonials</h2>
-
       <Swiper className="container testimonials__container"
-       // install Swiper modules
+      
       modules={[Pagination]}
       spaceBetween={40}
       slidesPerView={1}
       pagination={{ clickable: true }}
+
+      
       >
-        {
-          data.map(({avatar, name, review}, index) => {
-          return(
-         
-        <SwiperSlide  key={index} className="testimonial">
-          <div className="client__avatar">
-            <img src={avatar}/>
-         </div>
-            <h5 className='client__name'>{name}</h5>
-            <small className='client__review'>
-            {review}
-            </small>
-          
-        </SwiperSlide>
-          )
-           })
-         
-         }
+     
+{
+
+
+
+  data.map(({id, avatar, name, review}) =>(
+    <SwiperSlide key={id} className='testimonial'>
+    <div className='client__avatar'>
+      <img src={avatar} alt={name} />
+    </div>
+    <h5>{name}</h5>
+      <small className='client__review'>
+     {review}
+      </small>
+    </SwiperSlide>
+  ))
+}
         
       </Swiper>
+      
     </section>
   )
 }
 
-export default Testimonial
+export default Testimonials
